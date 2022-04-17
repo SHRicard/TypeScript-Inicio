311054b865377d70a074be926e3b253f38a46b92
SHRicard <SH_Ricardo@hotmail.com>
2022-04-16 21:53:38 -0300

    arreglo

--- src/hello.ts
+++ src/hello.ts
@@ -5,26 +5,3 @@ const i_love = () => {
     console.log("I love coding");
   }
 };
-
-console.log("hello asdads Pltzy !")
-
-const i_lovee = () => {
-  while (1 === 1) {
-    console.log("I love coding");
-  }
-};
-
-console.log("hello asdads Pltzy !")
-
-const i_lovef = () => {
-  while (1 === 1) {
-    console.log("I love coding");
-  }
-};
-console.log("hello asdads Pltzy !")
-
-const i_loveg = () => {
-  while (1 === 1) {
-    console.log("I love coding");
-  }
-};
